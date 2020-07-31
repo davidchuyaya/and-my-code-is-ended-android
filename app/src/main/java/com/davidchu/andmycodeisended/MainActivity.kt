43681cdb0be1f93864802fcb5223b3df405132d7
David Chu <davidchuyaya@gmail.com>
2020-07-31 13:31:07 -0700

    Date & time picker functional, saves

--- app/src/main/java/com/davidchu/andmycodeisended/MainActivity.kt
+++ app/src/main/java/com/davidchu/andmycodeisended/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.davidchu.andmycodeisended
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,18 +17,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setTimeDateText()
+        setTokenText()
+        createListeners()
+    }
+
+    private fun setTimeDateText() {
+        onTimeSwitchClicked(Settings.getAlarmOn(this)) //Enable/disable buttons accordingly
+        setTime(Settings.getAlarmTime(this))
+        setDate(Settings.getAlarmDate(this))
+    }
+    private fun setTime(time: LocalTime?) {
+        if (time == null) time_picker.text = getString(R.string.time_default)
+        else {
+            time_picker.text = time.format(DateTimeFormatter.ofPattern("hh:mm a"))
+            saveTimeDate(time, Settings.getAlarmDate(this) ?: LocalDate.now())
+        }
+    }
+    private fun setDate(date: LocalDate?) {
+        if (date == null) date_picker.text = getString(R.string.date_default)
+        else {
+            date_picker.text = date.format(DateTimeFormatter.ofPattern("MMMM d"))
+            saveTimeDate(Settings.getAlarmTime(this) ?: LocalTime.now(), date)
+        }
+    }
+    private fun saveTimeDate(time: LocalTime, date: LocalDate) {
+        val epoch = LocalDateTime.of(date, time).atZone(ZoneId.systemDefault()).toEpochSecond()
+        Settings.setAlarmEpoch(this, epoch)
+    }
+
+    private fun setTokenText() {
         TokenUtil.callWithNewToken {token ->
             if (token == null)
                 return@callWithNewToken
             code_dynamic_text.text = getString(R.string.code_dynamic_text, token)
             Settings.setToken(this, token)
         }
-
-        createListeners()
     }
 
     private fun createListeners() {
         code_clickable_area.setOnClickListener { onShareClicked() }
+        time_picker.setOnClickListener { TimePickerFragment().show(supportFragmentManager, "time")}
+        date_picker.setOnClickListener { DatePickerFragment().show(supportFragmentManager, "date")}
+        time_switch.setOnCheckedChangeListener { _, isChecked -> onTimeSwitchClicked(isChecked) }
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        when (fragment) {
+            is TimePickerFragment -> {
+                val time = Settings.getAlarmTime(this) ?: LocalTime.now()
+                fragment.setTimeAndCallback(time, ::setTime)
+            }
+            is DatePickerFragment -> {
+                val date = Settings.getAlarmDate(this) ?: LocalDate.now()
+                fragment.setDateAndCallback(date, ::setDate)
+            }
+        }
     }
 
     private fun onShareClicked() {
@@ -34,5 +84,11 @@ class MainActivity : AppCompatActivity() {
 
         val shareIntent = Intent.createChooser(sendIntent, null)
         startActivity(shareIntent)
+    }
+
+    private fun onTimeSwitchClicked(on: Boolean) {
+        time_picker.isEnabled = on
+        date_picker.isEnabled = on
+        Settings.setAlarmOn(this, on)
     }
 }
--- app/src/main/java/com/davidchu/andmycodeisended/Settings.kt
+++ app/src/main/java/com/davidchu/andmycodeisended/Settings.kt
@@ -1,10 +1,14 @@
 package com.davidchu.andmycodeisended
 
 import android.content.Context
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
 
 object Settings {
     enum class Keys {
-        Prefs, Token
+        Prefs, Token, Alarm, AlarmOn
     }
     fun getToken(context: Context): String? {
         return context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
@@ -14,6 +18,37 @@ object Settings {
         context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
             .edit()
             .putString(Keys.Token.name, token)
+            .apply()
+    }
+    fun getAlarmEpoch(context: Context): Long? {
+        val epoch = context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
+            .getLong(Keys.Alarm.name, -1)
+        return if (epoch != -1L) epoch else null
+    }
+    fun getAlarmTime(context: Context): LocalTime? {
+        val epoch = getAlarmEpoch(context)
+        return if (epoch == null) null else Instant.ofEpochSecond(epoch)
+            .atZone(ZoneId.systemDefault()).toLocalTime()
+    }
+    fun getAlarmDate(context: Context): LocalDate? {
+        val epoch = getAlarmEpoch(context)
+        return if (epoch == null) null else Instant.ofEpochSecond(epoch)
+            .atZone(ZoneId.systemDefault()).toLocalDate()
+    }
+    fun setAlarmEpoch(context: Context, epoch: Long) {
+        context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
+            .edit()
+            .putLong(Keys.Alarm.name, epoch)
+            .apply()
+    }
+    fun getAlarmOn(context: Context): Boolean {
+        return context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
+            .getBoolean(Keys.AlarmOn.name, true)
+    }
+    fun setAlarmOn(context: Context, on: Boolean) {
+        context.getSharedPreferences(Keys.Prefs.name, Context.MODE_PRIVATE)
+            .edit()
+            .putBoolean(Keys.AlarmOn.name, on)
             .apply()
     }
 }
